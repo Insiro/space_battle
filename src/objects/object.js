70c6bca97363fd5a428f6805b4366871bfc3b281
artisheep <artisheep@naver.com>
2022-11-05 01:17:35 +0900

    additional change

--- src/objects/object.js
+++ src/objects/object.js
@@ -14,7 +14,7 @@ export class Object {
         let diffx = Math.abs(this.x - object.x);
         let diffy = Math.abs(this.y - object.y);
         let diffz = Math.abs(this.z - object.z);
-        return diffx < 10 && diffy < 10 && diffz < 10;
+        return diffx < 5 && diffy < 5 && diffz < 5;
     }
     respawn(player) {}
 }
